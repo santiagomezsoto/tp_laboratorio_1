e22f2827f0212bd2857b1eb28ecfa06ee8d17963
Santiago <santiagomezsoto@gmail.com>
2016-05-24 21:32:13 -0300

    Rebuild

--- tp_laboratorio_1/funcionesUTN.h
+++
@@ -1,6 +0,0 @@
-int deseaContinuar();
-float sumar (float operando1, float operando2);
-float restar(float operando1, float operando2);
-float multiplicar(float operando1, float operando2);
-float dividir(float operando1, float operando2);
-int factorial(int numero);
